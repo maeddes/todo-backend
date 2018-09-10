4c560211fb06a9e6c6f9e2f07cdb3314a2149ede
Matthias Haeussler <maeddes@gmail.com>
2018-09-10 21:00:40 +0200

    CloudFoundry

--- src/main/java/de/maeddes/todolist/TodolistApplication.java
+++ src/main/java/de/maeddes/todolist/TodolistApplication.java
@@ -1,10 +1,15 @@
 package de.maeddes.todolist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @SpringBootApplication
@@ -12,10 +17,16 @@ import java.util.List;
 @RequestMapping("/todos")
 public class TodolistApplication {
 
-	ArrayList<String> todos = new ArrayList<>();
+	@Autowired
+	TodoRepository todoRepository;
 
 	@GetMapping
 	public List<String> getTodos(){
+
+		List<String> todos = new ArrayList<String>();
+
+		//for(Todo todo : todoRepository.findAll()) todos.add(todo.getTodo());
+		todoRepository.findAll().forEach(item->todos.add(item.getTodo()));
 
 		return todos;
 
@@ -24,7 +35,7 @@ public class TodolistApplication {
 	@PostMapping("{todo}")
 	public String add(@PathVariable String todo){
 
-		todos.add(todo);
+		todoRepository.save(new Todo(todo));
 		return "added "+todo;
 
 	}
@@ -32,7 +43,7 @@ public class TodolistApplication {
 	@DeleteMapping("{todo}")
 	public String remove(@PathVariable String todo){
 
-		todos.remove(todo);
+		todoRepository.deleteById(todo);
 		return "removed "+todo;
 
 	}
@@ -41,4 +52,33 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
+}
+
+
+@Entity
+class Todo{
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+	public Todo(String todo) {
+		this.todo = todo;
+	}
+
+	public Todo() {
+	}
+
+	@Id
+	String todo;
+
+}
+
+
+interface TodoRepository extends CrudRepository<Todo,String>{
+
 }
